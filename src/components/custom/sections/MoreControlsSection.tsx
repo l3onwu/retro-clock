af11a06f7754d69cc8ced8955901a8f615f9f233
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-01-06 14:34:42 +1100

    feat: add randomise colors

--- src/components/custom/sections/MoreControlsSection.tsx
+++ src/components/custom/sections/MoreControlsSection.tsx
@@ -1,10 +1,22 @@
-import { Flex, HStack, Button } from "@chakra-ui/react";
+import { useAppContext } from "@/lib/AppContext";
+import getRandomColor from "@/lib/utils/getRandomColor";
+import { Flex, HStack, Button, parseColor } from "@chakra-ui/react";
 
 export default function MoreControlsSection() {
+  const { setBodyColor, setDialColor, setHandsColor } = useAppContext();
+
+  const randomizeColors = () => {
+    setBodyColor(parseColor(getRandomColor()));
+    setDialColor(parseColor(getRandomColor()));
+    setHandsColor(parseColor(getRandomColor()));
+  };
+
   return (
     <Flex direction="column" height="25%">
       <HStack mb="10px">
-        <Button variant="subtle">Randomise</Button>
+        <Button variant="subtle" onClick={randomizeColors}>
+          Randomise
+        </Button>
         <Button variant="subtle">Screenshot</Button>
       </HStack>
       <Flex>
