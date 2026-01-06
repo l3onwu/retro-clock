7f29d2de35be56f69791431be067df0249692aa7
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-01-06 15:45:00 +1100

    refactor: remove 'public' from asset paths

--- src/lib/hooks/useClockModelView.ts
+++ src/lib/hooks/useClockModelView.ts
@@ -2,26 +2,20 @@ import { useLoader } from "@react-three/fiber";
 import { STLLoader } from "three/addons/loaders/STLLoader.js";
 
 export default function useClockModelView() {
-  const modelClockBody = useLoader(STLLoader, "/public/body_1.stl");
-  const modelClockBase = useLoader(STLLoader, "/public/base_1.stl");
-  const modelDialHours = useLoader(STLLoader, "/public/dial_hours_1.stl");
-  const modelDialSmile = useLoader(STLLoader, "/public/dial_smile_1.stl");
+  const modelClockBody = useLoader(STLLoader, "/body_1.stl");
+  const modelClockBase = useLoader(STLLoader, "/base_1.stl");
+  const modelDialHours = useLoader(STLLoader, "/dial_hours_1.stl");
+  const modelDialSmile = useLoader(STLLoader, "/dial_smile_1.stl");
   const modelHandsHoursChopsticks = useLoader(
     STLLoader,
-    "/public/hour_hand_chopsticks_1.stl"
+    "/hour_hand_chopsticks_1.stl"
   );
   const modelHandsMinutesChopsticks = useLoader(
     STLLoader,
-    "/public/minute_hand_chopsticks_1.stl"
+    "/minute_hand_chopsticks_1.stl"
   );
-  const modelHandsHoursBold = useLoader(
-    STLLoader,
-    "/public/hour_hand_bold_1.stl"
-  );
-  const modelHandsMinutesBold = useLoader(
-    STLLoader,
-    "/public/minute_hand_bold_1.stl"
-  );
+  const modelHandsHoursBold = useLoader(STLLoader, "/hour_hand_bold_1.stl");
+  const modelHandsMinutesBold = useLoader(STLLoader, "/minute_hand_bold_1.stl");
 
   // Center the geometry at origin
   modelClockBody.center();
--- src/lib/initialStates.ts
+++ src/lib/initialStates.ts
@@ -6,38 +6,32 @@ const initialStates = {
     {
       id: 1,
       name: "Full Markers",
-      image: "/public/image_dial_markers_1.png",
+      image: "/image_dial_markers_1.png",
       description: "Full hour markers with correct positions for all 12 hours.",
-      stlUrl: ["/public/dial_hours_1.stl"],
+      stlUrl: ["/dial_hours_1.stl"],
     },
     {
       id: 2,
       name: "Smile",
-      image: "/public/image_dial_smile_1.png",
+      image: "/image_dial_smile_1.png",
       description: "A smiley face design to bring joy to your timekeeping.",
-      stlUrl: ["/public/dial_smile_1.stl"],
+      stlUrl: ["/dial_smile_1.stl"],
     },
   ],
   handsDesignsImages: [
     {
       id: 1,
       name: "Chopsticks",
-      image: "/public/image_hands_chopsticks_1.png",
+      image: "/image_hands_chopsticks_1.png",
       description: "Elegant chopstick-shaped hands for a minimalist look.",
-      stlUrl: [
-        "/public/hour_hand_chopsticks_1.stl",
-        "/public/minute_hand_chopsticks_1.stl",
-      ],
+      stlUrl: ["/hour_hand_chopsticks_1.stl", "/minute_hand_chopsticks_1.stl"],
     },
     {
       id: 2,
       name: "Bold",
-      image: "/public/image_hands_bold_1.png",
+      image: "/image_hands_bold_1.png",
       description: "Thick and bold hands for easy readability.",
-      stlUrl: [
-        "/public/hour_hand_bold_1.stl",
-        "/public/minute_hand_bold_1.stl",
-      ],
+      stlUrl: ["/hour_hand_bold_1.stl", "/minute_hand_bold_1.stl"],
     },
   ],
 };
