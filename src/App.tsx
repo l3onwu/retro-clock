3d962a7582de01f6609b02bf7b4d2f2a5eebbb99
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-01-07 11:49:04 +1100

    style: add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Box } from "@chakra-ui/react";
 import PageContainer from "./components/custom/PageContainer";
 
 function App() {
   return (
-    <>
+    <Box height="100vh" width="100vw">
+      <Box
+        position="absolute"
+        top="-20px"
+        left="-20px"
+        right="-20px"
+        bottom="-20px"
+        bgImage="url('/backgrounds/texture10.jpg')"
+        bgSize="cover"
+        filter="blur(1px) saturate(100%) opacity(0.3) brightness(0.9)"
+        zIndex={-1}
+      />
       <PageContainer />
-    </>
+    </Box>
   );
 }
 
