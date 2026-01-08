0d21b8c0015bb45b2bd7218090c17754c8e66e53
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-01-08 13:53:19 +1100

    feat: add save name to save preset

--- src/components/custom/presets/SavePresetItem.tsx
+++ src/components/custom/presets/SavePresetItem.tsx
@@ -1,7 +1,15 @@
+import { useRef, useState } from "react";
 import { useAppContext } from "@/lib/contexts/AppContext";
 import type { PresetType } from "./PresetItem";
 import { savePresetToLocalStorage } from "@/lib/api/presetsLocalStorage";
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Dialog,
+  Button,
+  CloseButton,
+  Input,
+} from "@chakra-ui/react";
 
 export default function SavePresetItem() {
   const {
@@ -14,9 +22,21 @@ export default function SavePresetItem() {
     setSavedPresets,
   } = useAppContext();
 
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [newPresetName, setNewPresetName] = useState<string>("");
+  const initialRef = useRef<HTMLInputElement | null>(null);
+
+  const isValidName = (name: string) => {
+    return name.trim().length > 0;
+  };
+
+  const closeDialogHandler = () => {
+    setNewPresetName("");
+  };
+
   const savePreset = () => {
     const newPreset: PresetType = {
-      name: `Preset ${new Date().toLocaleTimeString()}`,
+      name: newPresetName.trim(),
       bodyColor: bodyColor.toString("hsl"),
       dialColor: dialColor.toString("hsl"),
       handsColor: handsColor.toString("hsl"),
@@ -27,26 +47,71 @@ export default function SavePresetItem() {
     savePresetToLocalStorage(newPreset);
     // Save to current state
     setSavedPresets([...savedPresets, newPreset]);
-    alert(
-      `Preset Saved!\n\nName: ${newPreset.name}\nBody Color: ${newPreset.bodyColor}\nDial Color: ${newPreset.dialColor}\nHands Color: ${newPreset.handsColor}\nDial Design: ${newPreset.dialDesign}\nHands Design: ${newPreset.handsDesign}`
-    );
+
+    // Close dialog
+    setDialogOpen(false);
   };
 
   return (
-    <Flex
-      flexShrink={0}
-      direction="column"
-      align="center"
-      justify="center"
-      width="150px"
-      height="90px"
-      border="1px solid #d4d4d4ff"
-      borderRadius="10px"
-      cursor="pointer"
-      onClick={savePreset}
-      _hover={{ scale: 1.05, boxShadow: "md" }}
+    <Dialog.Root
+      onExitComplete={closeDialogHandler}
+      open={dialogOpen}
+      onOpenChange={(e) => setDialogOpen(e.open)}
+      initialFocusEl={() => initialRef.current}
     >
-      <Text fontSize="xs">Save Preset</Text>
-    </Flex>
+      {/* Trigger */}
+      <Dialog.Trigger asChild>
+        <Flex
+          flexShrink={0}
+          direction="column"
+          align="center"
+          justify="center"
+          width="150px"
+          height="90px"
+          border="1px solid #d4d4d4ff"
+          borderRadius="10px"
+          cursor="pointer"
+          onClick={() => setDialogOpen(true)}
+          _hover={{ scale: 1.05, boxShadow: "md" }}
+        >
+          <Text fontSize="xs">Save Preset</Text>
+        </Flex>
+      </Dialog.Trigger>
+
+      {/* Content */}
+      <Dialog.Backdrop />
+      <Dialog.Positioner>
+        <Dialog.Content>
+          {/* Close */}
+          <Dialog.CloseTrigger asChild>
+            <CloseButton size="sm" />
+          </Dialog.CloseTrigger>
+          {/* Header */}
+          <Dialog.Header>
+            <Dialog.Title>Save preset</Dialog.Title>
+          </Dialog.Header>
+          {/* Body */}
+          <Dialog.Body>
+            <Flex width="100%" p={2} direction="row" gap={4}>
+              <Input
+                ref={initialRef}
+                placeholder="Preset Name"
+                value={newPresetName}
+                onChange={(e) => setNewPresetName(e.target.value)}
+              />
+              <Button
+                onClick={savePreset}
+                disabled={!isValidName(newPresetName)}
+                variant={"subtle"}
+              >
+                Save Preset
+              </Button>
+            </Flex>
+          </Dialog.Body>
+          {/* Footer */}
+          <Dialog.Footer />
+        </Dialog.Content>
+      </Dialog.Positioner>
+    </Dialog.Root>
   );
 }
