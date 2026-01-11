a90b1b4368c6f63e9b96f32f7e32e8f8e90583e8
Leon Wu <85681107+l3onwu@users.noreply.github.com>
2026-01-12 09:05:53 +1100

    refactor: delete unused backgrounds feature

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,6 @@ import PageContainer from "./components/custom/page/PageContainer";
 function App() {
   return (
     <Box height="100vh" width="100vw">
-      {/* <Box
-        position="absolute"
-        top="0px"
-        left="0px"
-        height="100%"
-        width="100%"
-        bgImage="url('/backgrounds/texture10.jpg')"
-        bgSize="cover"
-        filter="blur(1px) saturate(100%) opacity(0.3) brightness(0.9)"
-        zIndex={-1}
-      /> */}
       <PageContainer />
     </Box>
   );
